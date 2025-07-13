d777f248207b3e982035ba8351ccb3bd62c080d8
Ramatri <mramatri@prayag.ai>
2025-07-13 09:28:12 +0530

    Initial Commit

---
+++ Meet AI/Meet_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meet_AIApp.swift
+//  Meet AI
+//
+//  Created by Meet Ramatri on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Meet_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
